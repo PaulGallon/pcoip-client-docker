a746d98f6bda63c5da55ef711c3d2d24d22b31e2
Paul Gallon <paul.gallon@opia-sp.com>
2019-04-10 12:10:58 +0100

    Tidy up start.sh format

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
 SCRIPT_PATH=$(dirname "$(realpath $0)")
-docker run --rm -it -d -e DISPLAY=$DISPLAY -e QT_XKB_CONFIG_ROOT=/user/share/X11/xkb -v /tmp/.X11-unix:/tmp/.X11-unix --network=host -v teradici-client-config:/home/developer/.config `docker build -q -f ${SCRIPT_PATH}/Dockerfile ${SCRIPT_PATH}`
+
+docker run --rm -it -d \
+  -e DISPLAY=$DISPLAY \
+  -e QT_XKB_CONFIG_ROOT=/user/share/X11/xkb \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v teradici-client-config:/home/developer/.config \
+  --network=host \
+  `docker build -q -f ${SCRIPT_PATH}/Dockerfile ${SCRIPT_PATH}`
